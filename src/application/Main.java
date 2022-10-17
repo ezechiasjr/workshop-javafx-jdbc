f510c95b88de82e8a2ad64d6ca665e8c13c4b5b7
Ezechias Jr <ezechias.jr@gmail.com>
2022-10-17 12:21:04 -0300

    About View

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -19,7 +21,7 @@ public class Main extends Application {
 			scroolPane.setFitToHeight(true);
 			scroolPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scroolPane);
+			mainScene = new Scene(scroolPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -28,6 +30,10 @@ public class Main extends Application {
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
